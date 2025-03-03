f95a1723705df111c2dc12a9789a81e522ed5084
JanBu <yosefsitumorang97@gmail.com>
2025-03-03 14:32:40 +0700

    feat: loading table skeleton

--- app/(main)/analysis/_components/AnalysisTable.tsx
+++ app/(main)/analysis/_components/AnalysisTable.tsx
@@ -11,7 +11,8 @@ import {
 import { Button } from '@/components/ui/button'
 import useAnalysis from '@/hooks/use-analysis'
 import { ChevronRight, ChevronLeft, ArrowUp, ArrowDown } from 'lucide-react'
-import { BestWeatherData, WeatherData, WeatherResponse } from '@/utils/analysis'
+import { BestWeatherData } from '@/utils/analysis'
+import { Skeleton } from '@/components/ui/skeleton'
 
 const SORT_OPTIONS = [
   { key: 'yield', label: 'Yield' },
@@ -53,113 +54,117 @@ const AnalysisTable: React.FC<{
           </Button>
         ))}
       </div>
-      {loading ? (
-        <div>Loading...</div>
-      ) : (
-        <>
-          <Table>
-            <TableCaption className='text-start'>
-              A list of yield, reject due to pest, and reject due to diseases
-              based on weather data such as humidity, rain fall, and temperature
-              each week
-            </TableCaption>
-            <TableHeader>
-              <TableRow>
-                <TableHead className='text-start md:text-center'>
-                  Week Number
-                </TableHead>
-                <TableHead className='text-start md:text-center'>
-                  Humidity {`(%)`}
-                </TableHead>
-                <TableHead className='text-start md:text-center'>
-                  Rain Fall {`(mm)`}
-                </TableHead>
-                <TableHead className='text-start md:text-center'>
-                  Temperature {`(C)`}
-                </TableHead>
-                <TableHead className='text-start md:text-center'>
-                  Yield {`(kg)`}
-                </TableHead>
-                <TableHead className='text-start md:text-center'>
-                  Reject Due To Pest {`(kg)`}
-                </TableHead>
-                <TableHead className='text-start md:text-center'>
-                  Reject Due To Diseases {`(kg)`}
-                </TableHead>
-                <TableHead className='text-start md:text-center'>
-                  Total Reject {`(kg)`}
-                </TableHead>
+
+      <Table>
+        <TableCaption className='text-start'>
+          A list of yield, reject due to pest, and reject due to diseases based
+          on weather data such as humidity, rain fall, and temperature each week
+        </TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className='text-start md:text-center'>
+              Week Number
+            </TableHead>
+            <TableHead className='text-start md:text-center'>
+              Humidity {`(%)`}
+            </TableHead>
+            <TableHead className='text-start md:text-center'>
+              Rain Fall {`(mm)`}
+            </TableHead>
+            <TableHead className='text-start md:text-center'>
+              Temperature {`(C)`}
+            </TableHead>
+            <TableHead className='text-start md:text-center'>
+              Yield {`(kg)`}
+            </TableHead>
+            <TableHead className='text-start md:text-center'>
+              Reject Due To Pest {`(kg)`}
+            </TableHead>
+            <TableHead className='text-start md:text-center'>
+              Reject Due To Diseases {`(kg)`}
+            </TableHead>
+            <TableHead className='text-start md:text-center'>
+              Total Reject {`(kg)`}
+            </TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {loading ? (
+            Array.from({ length: 10 }).map((_, index) => (
+              <TableRow key={index}>
+                {Array.from({ length: 8 }).map((_, cellIndex) => (
+                  <TableCell key={cellIndex} className='text-center'>
+                    <Skeleton className='h-4 w-20 mx-auto' />
+                  </TableCell>
+                ))}
               </TableRow>
-            </TableHeader>
-            <TableBody>
-              {data?.content.length === 0 ? (
-                <tr>
-                  <td colSpan={7} className='text-center py-4'>
-                    No data available
-                  </td>
-                </tr>
-              ) : (
-                data?.content.map((content) => (
-                  <TableRow
-                    key={content.weekNumber}
-                    className={`${
-                      content.weekNumber === bestWeather?.weekNumber
-                        ? 'bg-green-500'
-                        : ''
-                    }`}
-                  >
-                    <TableCell className='font-medium text-center'>
-                      {content.weekNumber}
-                    </TableCell>
-                    <TableCell className='font-medium text-center'>
-                      {content.humidity}
-                    </TableCell>
-                    <TableCell className='font-medium text-center'>
-                      {content.rainFall}
-                    </TableCell>
-                    <TableCell className='font-medium text-center'>
-                      {content.temperature}
-                    </TableCell>
-                    <TableCell className='font-medium text-center'>
-                      {content.totalYield}
-                    </TableCell>
-                    <TableCell className='font-medium text-center'>
-                      {content.rejectDueToPest}
-                    </TableCell>
-                    <TableCell className='font-medium text-center'>
-                      {content.rejectDueToDisease}
-                    </TableCell>
-                    <TableCell className='font-medium text-center'>
-                      {content.totalReject}
-                    </TableCell>
-                  </TableRow>
-                ))
-              )}
-            </TableBody>
-          </Table>
-          <div className='flex items-center justify-between mt-4'>
-            <div>
-              Page {data?.number! + 1} of {data?.totalPages}
-            </div>
-            <div className='space-x-4'>
-              <Button
-                variant='outline'
-                disabled={data?.first}
-                onClick={() => refetch({ page: (data?.number || 1) - 1 })}
+            ))
+          ) : data?.content.length === 0 ? (
+            <tr>
+              <td colSpan={8} className='text-center py-4'>
+                No data available
+              </td>
+            </tr>
+          ) : (
+            data?.content.map((content) => (
+              <TableRow
+                key={content.weekNumber}
+                className={
+                  content.weekNumber === bestWeather?.weekNumber
+                    ? 'bg-green-500'
+                    : ''
+                }
               >
-                <ChevronLeft />
-              </Button>
-              <Button
-                variant='outline'
-                disabled={data?.last}
-                onClick={() => refetch({ page: (data?.number || 0) + 1 })}
-              >
-                <ChevronRight />
-              </Button>
-            </div>
-          </div>
-        </>
-      )}
+                <TableCell className='font-medium text-center'>
+                  {content.weekNumber}
+                </TableCell>
+                <TableCell className='font-medium text-center'>
+                  {content.humidity}
+                </TableCell>
+                <TableCell className='font-medium text-center'>
+                  {content.rainFall}
+                </TableCell>
+                <TableCell className='font-medium text-center'>
+                  {content.temperature}
+                </TableCell>
+                <TableCell className='font-medium text-center'>
+                  {content.totalYield}
+                </TableCell>
+                <TableCell className='font-medium text-center'>
+                  {content.rejectDueToPest}
+                </TableCell>
+                <TableCell className='font-medium text-center'>
+                  {content.rejectDueToDisease}
+                </TableCell>
+                <TableCell className='font-medium text-center'>
+                  {content.totalReject}
+                </TableCell>
+              </TableRow>
+            ))
+          )}
+        </TableBody>
+      </Table>
+      <div className='flex items-center justify-between mt-4'>
+        <div>
+          Page {data?.number! + 1} of {data?.totalPages}
+        </div>
+        <div className='space-x-4'>
+          <Button
+            variant='outline'
+            disabled={data?.first}
+            onClick={() => refetch({ page: (data?.number || 1) - 1 })}
+          >
+            <ChevronLeft />
+          </Button>
+          <Button
+            variant='outline'
+            disabled={data?.last}
+            onClick={() => refetch({ page: (data?.number || 0) + 1 })}
+          >
+            <ChevronRight />
+          </Button>
+        </div>
+      </div>
     </>
   )
 }
